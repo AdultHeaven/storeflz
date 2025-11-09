53cfdb79d0c8a7a96bd542ac0aacd67374495a7b
Frank <frankforflirt@gmail.com>
2025-11-09 07:50:08 +0530

    juicy Ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,20 +24,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   );
 }
 
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-}
