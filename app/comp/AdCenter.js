d4c677563e1660fc29b6f067cb378a73a8591f82
Frank <frankforflirt@gmail.com>
2026-02-20 23:56:12 +0530

    Exo to Hilltop

--- app/comp/AdCenter.js
+++ app/comp/AdCenter.js
@@ -1,38 +1,84 @@
+// 'use client';
+// import { useEffect } from 'react';
+
+// export default function AdCenter() {
+//   useEffect(() => {
+//     // Load ad-provider.js script
+//     const script = document.createElement("script");
+//     script.src = "https://a.magsrv.com/ad-provider.js";
+//     script.async = true;
+//     script.type = "application/javascript";
+//     document.body.appendChild(script);
+
+//     // Push the ad serve call
+//     window.AdProvider = window.AdProvider || [];
+//     window.AdProvider.push({ serve: {} });
+
+//     return () => {
+//       document.body.removeChild(script);
+//     };
+//   }, []);
+
+//   return (
+//     <div 
+//       style={{
+//         width: "100%",
+//         display: "flex",
+//         justifyContent: "center",
+//         marginTop: "20px"
+//       }}
+//     >
+//       <ins 
+//         className="eas6a97888e2" 
+//         data-zoneid="5777364"
+//         style={{ display: "block" }}
+//       ></ins>
+//     </div>
+//   );
+// }
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
-export default function AdCenter() {
+export default function HilltopAd() {
+  const adRef = useRef(null);
+
   useEffect(() => {
-    // Load ad-provider.js script
-    const script = document.createElement("script");
-    script.src = "https://a.magsrv.com/ad-provider.js";
-    script.async = true;
-    script.type = "application/javascript";
-    document.body.appendChild(script);
+    if (!adRef.current) return;
 
-    // Push the ad serve call
-    window.AdProvider = window.AdProvider || [];
-    window.AdProvider.push({ serve: {} });
+    const script = document.createElement('script');
+
+    script.innerHTML = `
+      (function(vqexh){
+        var d = document,
+            s = d.createElement('script'),
+            l = document.currentScript;
+        s.settings = vqexh || {};
+        s.src = "//amazingmeaning.com/b/XcV.sadOGhlA0/YiWxch/RehmC9/uZZ/UglpkwPTTUYS4EMuTpUP4tMFj-U/tZNWjngOx/NdTCgRybOnQx";
+        s.async = true;
+        s.referrerPolicy = 'no-referrer-when-downgrade';
+        l.parentNode.insertBefore(s, l);
+      })({});
+    `;
+
+    adRef.current.appendChild(script);
 
     return () => {
-      document.body.removeChild(script);
+      if (adRef.current?.contains(script)) {
+        adRef.current.removeChild(script);
+      }
     };
   }, []);
 
   return (
-    <div 
+    <div
       style={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        marginTop: "20px"
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '20px',
       }}
     >
-      <ins 
-        className="eas6a97888e2" 
-        data-zoneid="5777364"
-        style={{ display: "block" }}
-      ></ins>
+      <div ref={adRef} />
     </div>
   );
 }
