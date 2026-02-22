790b72b6f88c795515d329c5d4f584a9a6e666e8
Frank <frankforflirt@gmail.com>
2026-02-22 06:11:42 +0530

    Sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -2,7 +2,7 @@ export async function GET() {
   const workerUrl = "https://storeflz-sitemap.ahapi.workers.dev/sitemap.xml";
 
   const response = await fetch(workerUrl, {
-    next: { revalidate: 3600 }, // 1 hour cache
+    next: { revalidate: 0 }, // 1 hour cache
   });
 
   const xml = await response.text();
