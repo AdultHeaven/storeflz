05200e4961b23e1ce9aa93d3aef63fa112810949
Frank <frankforflirt@gmail.com>
2026-02-27 18:29:35 +0530

    No Cache

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,8 +1,17 @@
 export async function GET() {
-  const workerUrl = "https://storeflz-sitemap.ahapi.workers.dev/sitemap.xml";
+  // const workerUrl = "https://storeflz-sitemap.ahapi.workers.dev/sitemap.xml";
 
-  const response = await fetch(workerUrl);
+  // const response = await fetch(workerUrl);
+const workerUrl = "https://storeflz-sitemap.ahapi.workers.dev/sitemap.xml";
 
+const response = await fetch(workerUrl, {
+  method: "GET",
+  cache: "no-store", // tells browser not to use cache
+  headers: {
+    "Cache-Control": "no-store",
+    "Pragma": "no-cache"
+  }
+});
   const xml = await response.text();
 
   return new Response(xml, {
