b375fc18964c9daf03b528aae389cbd5b3d80256
Frank <frankforflirt@gmail.com>
2026-03-11 10:08:14 +0530

    Dev Note Removed

--- app/file/[id]/page.tsx
+++ app/file/[id]/page.tsx
@@ -148,7 +148,7 @@ export default async function FileViewPage({ params }: { params: { id: string } 
 
           {/* interactive part */}
           <MirrorList mirrors={data.mirrors} pageUrl={pageUrl} />
-          <div className="dev-notice">
+          {/* <div className="dev-notice">
   <div className="dev-notice-icon">⚠️</div>
 
   <div className="dev-notice-text">
@@ -156,7 +156,7 @@ export default async function FileViewPage({ params }: { params: { id: string } 
     <p>
 Some links may not open on the first click. Please click again if needed. We're working on improving this and will update it soon. Thank you for your patience.    </p>
   </div>
-</div>
+</div> */}
         </section>
       </main>
       {/* <AdCenter/> */}
