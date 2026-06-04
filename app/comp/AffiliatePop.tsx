7ee32c70aa6ea554b49e7763068d874e74ecabd6
Frank <frankforflirt@gmail.com>
2026-06-04 21:26:36 +0530

    Dating Pop

--- app/comp/AffiliatePop.tsx
+++ app/comp/AffiliatePop.tsx
@@ -1,3 +1,30 @@
+// 'use client';
+
+// import Script from 'next/script';
+
+// export default function AffiliatePop() {
+//   return (
+//     <>
+//       <Script
+//         src="https://crxcra.com/popin/latest/affstitial-min.js"
+//         strategy="afterInteractive"
+//       />
+
+//       <Script id="crak-popin" strategy="afterInteractive">
+//         {`
+//           var crakPopInParamsOverlay = {
+//             url: 'https://t.vlmai-1.com/384478/9022/38565?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
+//             decryptUrl: false,
+//             contentType: 'overlay',
+//             coverOverlay: true,
+//             expireDays: 0.01
+//           };
+//         `}
+//       </Script>
+//     </>
+//   );
+// }
+
 'use client';
 
 import Script from 'next/script';
@@ -13,7 +40,7 @@ export default function AffiliatePop() {
       <Script id="crak-popin" strategy="afterInteractive">
         {`
           var crakPopInParamsOverlay = {
-            url: 'https://t.vlmai-1.com/384478/9022/38565?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
+            url: 'https://t.crdtg3.com/384478/5165?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
             decryptUrl: false,
             contentType: 'overlay',
             coverOverlay: true,
