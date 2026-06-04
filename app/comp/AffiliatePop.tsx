0a73d4b3626a897488cac8ac9b112aa627ee899d
Frank <frankforflirt@gmail.com>
2026-06-04 20:59:07 +0530

    Candi Pop

--- app/comp/AffiliatePop.tsx
+++ app/comp/AffiliatePop.tsx
@@ -6,19 +6,19 @@ export default function AffiliatePop() {
   return (
     <>
       <Script
-        src="//static.scptp9.com/mnpw3.js"
+        src="https://crxcra.com/popin/latest/affstitial-min.js"
         strategy="afterInteractive"
       />
 
-      <Script id="mnpw-pop" strategy="afterInteractive">
+      <Script id="crak-popin" strategy="afterInteractive">
         {`
-          mnpw.add(
-            'https://t.vlmai-1.com/384478/7793?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0005&pud=scptp9',
-            {
-              newTab: true,
-              cookieExpires: 86401
-            }
-          );
+          var crakPopInParamsOverlay = {
+            url: 'https://t.vlmai-1.com/384478/9022/38565?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
+            decryptUrl: false,
+            contentType: 'overlay',
+            coverOverlay: true,
+            expireDays: 0.01
+          };
         `}
       </Script>
     </>
