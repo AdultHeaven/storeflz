0dcf2a09d72f79e0788f426ad07d9a88ffe09b3b
Frank <frankforflirt@gmail.com>
2026-06-05 07:44:29 +0530

    Candi Pop

--- app/comp/AffiliatePop.tsx
+++ app/comp/AffiliatePop.tsx
@@ -1,29 +1,3 @@
-// 'use client';
-
-// import Script from 'next/script';
-
-// export default function AffiliatePop() {
-//   return (
-//     <>
-//       <Script
-//         src="https://crxcra.com/popin/latest/affstitial-min.js"
-//         strategy="afterInteractive"
-//       />
-
-//       <Script id="crak-popin" strategy="afterInteractive">
-//         {`
-//           var crakPopInParamsOverlay = {
-//             url: 'https://t.vlmai-1.com/384478/9022/38565?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
-//             decryptUrl: false,
-//             contentType: 'overlay',
-//             coverOverlay: true,
-//             expireDays: 0.01
-//           };
-//         `}
-//       </Script>
-//     </>
-//   );
-// }
 'use client';
 
 import Script from 'next/script';
@@ -39,7 +13,7 @@ export default function AffiliatePop() {
       <Script id="crak-popin" strategy="afterInteractive">
         {`
           var crakPopInParamsOverlay = {
-            url: 'https://t.acrsmartcam.com/384478/3778/0?po=6533&aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
+            url: 'https://t.vlmai-1.com/384478/9022/38565?aff_sub5=SF_006OG000004lmDN&aff_sub4=AT_0019',
             decryptUrl: false,
             contentType: 'overlay',
             coverOverlay: true,
