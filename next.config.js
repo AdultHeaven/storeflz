6d30c9771d541a79b9e1072fe2c1e4e553076277
Frank <frankforflirt@gmail.com>
2026-06-28 01:46:28 +0530

    Caching

--- next.config.js
+++ next.config.js
@@ -1,15 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
+    const cacheHeaders = [
+      {
+        key: 'Cache-Control',
+        value: 'public, max-age=2592000, s-maxage=2592000, stale-while-revalidate=86400',
+      },
+      {
+        key: 'Vercel-CDN-Cache-Control',
+        value: 'public, max-age=2592000, s-maxage=2592000, stale-while-revalidate=86400',
+      },
+      {
+        key: 'CDN-Cache-Control',
+        value: 'public, max-age=2592000, s-maxage=2592000, stale-while-revalidate=86400',
+      },
+    ];
+
     return [
       {
+        source: '/',
+        headers: cacheHeaders,
+      },
+      {
+        source: '/report',
+        headers: cacheHeaders,
+      },
+      {
         source: '/file/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=2592000, s-maxage=2592000, stale-while-revalidate=86400',
-          },
-        ],
+        headers: cacheHeaders,
+      },
+      {
+        source: '/folder/:path*',
+        headers: cacheHeaders,
       },
     ];
   },
