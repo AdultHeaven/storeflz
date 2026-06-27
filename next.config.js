9a8bd23c7accf5c9c6c9fa7c8c47f5c954545adf
Frank <frankforflirt@gmail.com>
2026-06-28 01:04:46 +0530

    Caching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/file/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, s-maxage=2592000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
